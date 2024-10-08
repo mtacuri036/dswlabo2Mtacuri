d5ff531bfb333610ff2f6f6e0579d79239d526bb
mtacuri036 <mateo.tacuri@epn.edu.ec>
2024-10-08 00:28:21 -0500

    Add files via upload

---
+++ API/fundamentos.js
@@ -0,0 +1,27 @@
+
+// variables
+
+// let y const
+
+//const algo fijo que no cambia
+
+// tipos de datos
+let data1=42; 
+let data2= 2.5;
+let data3="Mateo" 
+let data4=false
+let data5; 
+let data6 ='@' //char
+let data7 = null //null
+
+console.log(data2);
+
+
+//tipos de datos
+let data8=["Mateo", "Juan", "Marcelo"]; //Array
+
+// objeto
+let data9= datos={
+    name:"Mateo",
+    city:"Quito"
+}
